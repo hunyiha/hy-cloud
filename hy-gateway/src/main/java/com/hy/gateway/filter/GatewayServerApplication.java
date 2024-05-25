f587d66f2a834f2b513c169716778a5509358821
guangpeng.he1 <hunyiha@gmail.com>
2024-05-26 01:27:37 +0800

    初始化项目

---
+++ hy-gateway/src/main/java/com/hy/gateway/filter/GatewayServerApplication.java
@@ -0,0 +1,16 @@
+package com.hy.gateway.filter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: hunyiha
+ * @create-date: 2024-05-25 11:48
+ */
+
+@SpringBootApplication
+public class GatewayServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServerApplication.class, args);
+    }
+}
